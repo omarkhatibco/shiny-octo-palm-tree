85ecaa19ef51b25944cb632ea51a60a0db2bfa5c
Omar Khatib <omar.khatib@tourlane.com>
2024-03-22 22:44:32 +0100

    feat: add chakra ui and delete old code

--- src/contexts/providers.tsx
+++ src/contexts/providers.tsx
@@ -1,7 +1,14 @@
+'use client'
+
 import { PropsWithChildren } from 'react'
 import { client } from '@/utls'
+import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'urql'
 
 export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
-  return <Provider value={client}>{children}</Provider>
+  return (
+    <Provider value={client}>
+      <ChakraProvider>{children}</ChakraProvider>
+    </Provider>
+  )
 }
