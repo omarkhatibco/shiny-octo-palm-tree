c302979c5e6304a9063ee62b03b342284b470f33
Omar Khatib <omar.khatib@tourlane.com>
2024-03-24 15:58:42 +0100

    feat: add app title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
-'use client'
-
-import { useGetCharectersQuery, useGetEpisodesQuery } from '@/graphql/generated'
 import { Button } from '@chakra-ui/react'
 
 export default function Page() {
-  const [result] = useGetCharectersQuery({ variables: { page: 2 } })
-  const [eps] = useGetEpisodesQuery()
-  console.log({ result })
-  console.log({ eps })
   return (
     <main>
       hello Perdoo! <Button>hello Perdoo! </Button>
